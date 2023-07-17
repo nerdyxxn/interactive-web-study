0cb5a2669ce8ffac4d6906fb2207d4f444838b33
윤성 <yunyul1209@gmail.com>
2023-07-17 17:25:40 +0900

    feat : 3d-wall resizeHandler 적용
    - 창크기 변경될 때마다 maxScrollValue 갱신되도록 처리

--- Interactive_section11-3d-wall/js/wall3d.js
+++ Interactive_section11-3d-wall/js/wall3d.js
@@ -1,11 +1,19 @@
 (function () {
   // house를 이동시켜서 스크롤 했을 때 wall 움직이도록 구현
   const houseElem = document.querySelector('.house');
-  let maxScrollValue = document.body.offsetHeight - window.innerHeight; // 전체 문서 높이 - 현재 창 높이 = 스크롤 범위
+  let maxScrollValue;
+
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight; // 전체 문서 높이 - 현재 창 높이 = 스크롤 범위
+  }
 
   window.addEventListener('scroll', function () {
     // console.log(scrollY / maxScrollValue); : 얼마나 스크롤 했는지 비율로 구하기
     const zMove = (scrollY / maxScrollValue) * 980 - 490;
     houseElem.style.transform = `translateZ(${zMove}vw)`; // 'translateZ(' + zMove + 'vw)';
   });
+
+  // 창크기 변경될 때마다 maxScrollValue 갱신되도록 처리해야 사이즈 변동되도 정상 작동함
+  window.addEventListener('resize', resizeHandler);
+  resizeHandler();
 })();
