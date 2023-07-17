2529a5516432cf685532cfb14874899de8e79f3f
윤성 <yunyul1209@gmail.com>
2023-07-17 17:41:24 +0900

    feat : 3d-wall progress-bar 적용
    - 스크롤 비율만큼 progress-bar width 채워지도록 처리

--- Interactive_section11-3d-wall/js/wall3d.js
+++ Interactive_section11-3d-wall/js/wall3d.js
@@ -1,6 +1,7 @@
 (function () {
   // house를 이동시켜서 스크롤 했을 때 wall 움직이도록 구현
   const houseElem = document.querySelector('.house');
+  const barElem = document.querySelector('.progress-bar');
   let maxScrollValue;
 
   function resizeHandler() {
@@ -9,8 +10,12 @@
 
   window.addEventListener('scroll', function () {
     // console.log(scrollY / maxScrollValue); : 얼마나 스크롤 했는지 비율로 구하기
-    const zMove = (scrollY / maxScrollValue) * 980 - 490;
+    const scrollPer = scrollY / maxScrollValue;
+    const zMove = scrollPer * 980 - 490;
     houseElem.style.transform = `translateZ(${zMove}vw)`; // 'translateZ(' + zMove + 'vw)';
+
+    // 스크롤 비율만큼 progress-bar width 채워지도록 처리
+    barElem.style.width = scrollPer * 100 + '%';
   });
 
   // 창크기 변경될 때마다 maxScrollValue 갱신되도록 처리해야 사이즈 변동되도 정상 작동함
