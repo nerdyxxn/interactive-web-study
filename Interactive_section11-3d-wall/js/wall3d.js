961e9c967bb2e3d31271258b3490a68e59aac103
윤성 <yunyul1209@gmail.com>
2023-07-18 18:39:17 +0900

    feat : 3d-wall 마우스 위치에 따라 stage 회전시켜서 시점 변환

--- Interactive_section11-3d-wall/js/wall3d.js
+++ Interactive_section11-3d-wall/js/wall3d.js
@@ -1,7 +1,8 @@
 (function () {
-  // house를 이동시켜서 스크롤 했을 때 wall 움직이도록 구현
+  const stageElem = document.querySelector('.stage');
   const houseElem = document.querySelector('.house');
   const barElem = document.querySelector('.progress-bar');
+  const mousePos = { x: 0, y: 0 };
   let maxScrollValue;
 
   function resizeHandler() {
@@ -9,6 +10,7 @@
   }
 
   window.addEventListener('scroll', function () {
+    // house를 이동시켜서 스크롤 했을 때 wall 움직이도록 구현
     // console.log(scrollY / maxScrollValue); : 얼마나 스크롤 했는지 비율로 구하기
     const scrollPer = scrollY / maxScrollValue;
     const zMove = scrollPer * 980 - 490;
@@ -16,6 +18,17 @@
 
     // 스크롤 비율만큼 progress-bar width 채워지도록 처리
     barElem.style.width = scrollPer * 100 + '%';
+  });
+
+  // 마우스 위치에 따라 stage 회전시켜서 시점 변환
+  window.addEventListener('mousemove', function (e) {
+    // e.clientX, e.clientY : 마우스 위치 가져오기
+    // 중심 기준을 0으로 잡고 왼쪽으로 이동하면 -1에 수렴, 오른쪽으로 이동하면 +1에 수렴
+    mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+    mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+
+    stageElem.style.transform =
+      'rotateX(' + mousePos.y * 5 + 'deg) rotateY(' + mousePos.x * 5 + 'deg)';
   });
 
   // 창크기 변경될 때마다 maxScrollValue 갱신되도록 처리해야 사이즈 변동되도 정상 작동함
