878336590faaaca50fa048a8a3fa05e666252d61
윤성 <yunyul1209@gmail.com>
2023-07-26 17:14:18 +0900

    feat : 3d-wall 스크롤 했을 때 캐릭터가 걷는 이벤트 추가

--- Interactive_section11-3d-wall/js/Character.js
+++ Interactive_section11-3d-wall/js/Character.js
@@ -32,4 +32,20 @@ function Character(info) {
   document.querySelector('.stage').appendChild(this.mainElem);
 
   this.mainElem.style.left = info.xPos + '%';
+  this.init();
 }
+
+// 스크롤 했을 때 Character에 running 클래스 추가하는 메소드 작성
+// 객체가 공통으로 사용하는 속성이나 메소드는 prototype 객체에 만들기
+Character.prototype = {
+  constructor: Character,
+  init: function () {
+    // 이벤트 핸들러 내부에서 this를 사용하면 this = window를 가리킴
+    // 우리는 window가 아닌 Character에 접근해야 하기 때문에 이벤트 핸들러 밖에서 미리 this를 변수에 담아둔 다음에 사용함
+    const self = this;
+
+    window.addEventListener('scroll', function () {
+      self.mainElem.classList.add('running');
+    });
+  },
+};
