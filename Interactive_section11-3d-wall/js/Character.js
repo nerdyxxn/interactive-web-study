2a2cc1f7961972542c397d4530905824d4e7b3b8
윤성 <yunyul1209@gmail.com>
2023-07-27 16:12:11 +0900

    feat : 키 누르면 speed 값만큼씩 좌우로 이동하도록 구현
    - requestAnimationFrame 메소드 활용하여 캐릭터 부드럽게 이동

--- Interactive_section11-3d-wall/js/Character.js
+++ Interactive_section11-3d-wall/js/Character.js
@@ -37,6 +37,17 @@ function Character(info) {
   this.scrollState = false;
   // 마지막 스크롤 위치
   this.lastScrollTop = 0;
+
+  this.xPos = info.xPos;
+  this.speed = 0.3;
+
+  // 방향 설정
+  this.direction;
+  // 좌우 이동 중인지 아닌지 확인
+  this.runningState = false;
+  // requestAnimationFrame가 리턴하는 값 저장할 속성
+  this.rafId;
+
   this.init();
 }
 
@@ -57,7 +68,6 @@ Character.prototype = {
 
       if (!self.scrollState) {
         self.mainElem.classList.add('running');
-        console.log('running 붙~~~!');
       }
 
       self.scrollState = setTimeout(function () {
@@ -79,5 +89,79 @@ Character.prototype = {
 
       self.lastScrollTop = scrollY;
     });
+
+    // 키보드 키마다 고유의 keycode값 존재 -> left: 37 / right: 39
+    // 이를 활용해 캐릭터의 좌우 움직임 구현
+    window.addEventListener('keydown', function (e) {
+      if (self.runningState) return;
+
+      if (e.key === 'ArrowLeft') {
+        // 왼쪽
+        self.direction = 'left';
+        self.mainElem.setAttribute('data-direction', 'left');
+        self.mainElem.classList.add('running');
+        self.run(self);
+        self.runningState = true;
+      } else if (e.key === 'ArrowRight') {
+        // 오른쪽
+        self.direction = 'right';
+        self.mainElem.setAttribute('data-direction', 'right');
+        self.mainElem.classList.add('running');
+        self.run(self);
+        self.runningState = true;
+      }
+    });
+
+    window.addEventListener('keyup', function (e) {
+      self.mainElem.classList.remove('running');
+
+      // requestAnimationFrame cancle 처리
+      cancelAnimationFrame(self.rafId);
+    });
   },
+  // 키 누르면 speed 값만큼씩 이동하도록 구현
+  run: function (self) {
+    if (self.direction == 'left') {
+      self.xPos -= self.speed;
+    } else if (self.direction == 'right') {
+      self.xPos += self.speed;
+    }
+
+    if (self.xPos < 2) {
+      self.xPos = 2;
+    }
+
+    if (self.xPos > 88) {
+      self.xPos = 88;
+    }
+
+    self.mainElem.style.left = self.xPos + '%';
+
+    self.rafId = requestAnimationFrame(function () {
+      self.run(self);
+    });
+  },
+
+  // bind를 사용한 방법
+  // run: function () {
+  //     const self = this;
+  //
+  //     if (self.direction == 'left') {
+  //         self.xPos -= self.speed;
+  //     } else if (self.direction == 'right') {
+  //         self.xPos += self.speed;
+  //     }
+  //
+  //     if (self.xPos < 2) {
+  //         self.xPos = 2;
+  //     }
+  //
+  //     if (self.xPos > 88) {
+  //         self.xPos = 88;
+  //     }
+  //
+  //     self.mainElem.style.left = self.xPos + '%';
+  //
+  //     self.rafId = requestAnimationFrame(self.run.bind(self));
+  // }
 };
